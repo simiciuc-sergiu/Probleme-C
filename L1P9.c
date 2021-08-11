1601e5b4fa7e96ec762767ab0707a3520d55dfee
Sergiu Simiciuc <serjsym@gmail.com>
2021-08-11 19:00:08 +0300

    Laborator 1

---
+++ L1P9.c
@@ -0,0 +1,23 @@
+void main()
+{
+	unsigned char x = 0;
+	unsigned short y = 0;
+	unsigned long z = 0;
+	char a = 0;
+	short b = 0;
+	long c = 0;
+	x = 260;
+	printf("%d \n",x); //4
+	y = 100000;
+	printf("%d \n",y); //34463 
+	z = 4302548562;
+	printf("%d \n",z); //7581266
+
+
+	a = 300;
+	printf("%d \n",a); //44
+	b = 45000;
+	printf("%d \n",b); //-20536
+	c = 2501254856;
+	printf("%d \n",c); //-1793712440
+}
